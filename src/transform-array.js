23bfb709d090dc8bb28bef6a0f4740f992c16c3b
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2022-05-03 00:13:49 +0300

    feat: add task transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform( arr) {
+ function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let arrayTransform = arr
+  let total = [];
 
+  arrayTransform.forEach((value, index) => {
+    if (value == '--discard-next') {
+      delete arr[index + 1]
+    }
+    else if (value == '--discard-prev') {
+      if (total[index - 1] == arr[index - 1]) {
+        delete total[index - 1]
+      }
+      delete arr[index - 1]
+    }
+    else if (value == '--double-next') {
+      if (arr[index + 1] != undefined) {
+        total.push(arr[index + 1])
+      }
+
+    }
+    else if (value == '--double-prev') {
+      if (arr[index - 1] != undefined)
+      total.push(arr[index - 1])
+
+    } else if (value != undefined) {
+      total.push(value)
+    }
+  })
+  let totality = total.filter(value => value != undefined);
+  return totality
 }
 
 
+// function transform(arr) {
+//   if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+
+//   let result = [];
+
+//   arr.forEach((elem, index) => {
+//     if (elem == '--discard-next') {
+//       delete arr[index + 1]
+//     }
+//     else if (elem == '--discard-prev') {
+//       if (result[index - 1] == arr[index - 1]) {
+//         delete result[index - 1]
+//       }
+//       delete arr[index - 1]
+//     }
+//     else if (elem == '--double-next') {
+//       if (arr[index + 1] != undefined) {
+//         result.push(arr[index + 1])
+//       }
+
+//     }
+//     else if (elem == '--double-prev') {
+//       if (arr[index - 1] != undefined)
+//         result.push(arr[index - 1])
+
+//     } else if (elem != undefined) {
+//       result.push(elem)
+//     }
+//   })
+
+//   return result.filter(item => item != undefined);
+// }
 
 
 
